ea084578ad23a5532aeff30d67cdf7633bdc3815
ISHA-AGARWAL-19 <92115783+ISHA-AGARWAL-19@users.noreply.github.com>
2023-05-06 14:26:59 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,15 +1,8 @@
 import pandas as pd
 
 def preprocess(df,region_df):
+    df = df[df['Season'] == "Summer"]
     df = df.merge(region_df, on='NOC', how='left')
     df.drop_duplicates(inplace=True)
     df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
     return df
-
-def preprocess_s(df1, df2 , region):
-    df= pd.concat([df1,df2])
-    df = df.merge(region, on='NOC', how='left')
-    df.drop_duplicates(inplace=True)
-    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
-    return df
-
